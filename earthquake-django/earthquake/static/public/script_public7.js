a12a723bafce52e429d48a32725fa1f9337c0cc7
Mari Leonhardsen <marileonhardsen@Maris-MacBook-Air.local>
2018-04-26 17:41:58 -0700

    finish public style

--- earthquake-django/earthquake/static/public/script_public7.js
+++ earthquake-django/earthquake/static/public/script_public7.js
@@ -247,7 +247,7 @@ function select_earthquake(e) {
         */
     };
     
-    e.style.background = 'black';
+    e.style.background = '#333333';
     e.style.color = 'white';
     //e.getElementsByClassName('table_row')[0].style.background = 'black';
     //e.getElementsByClassName('table_row')[1].style.background = 'black';
@@ -277,24 +277,5 @@ function speed_x(e) {
 };
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+
 
